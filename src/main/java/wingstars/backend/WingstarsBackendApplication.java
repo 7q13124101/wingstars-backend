bfb7b0bfc2c3cf32de3b9e34b13a6bf0c106eaf9
台鋼_金小士 <jinxiaoshi24@gmail.com>
2026-04-01 10:38:54 +0800

    initial Spring Boot wingstars backend project

---
+++ src/main/java/wingstars/backend/WingstarsBackendApplication.java
@@ -0,0 +1,13 @@
+package wingstars.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingstarsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WingstarsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wingstars/backend/WingstarsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wingstars.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WingstarsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
