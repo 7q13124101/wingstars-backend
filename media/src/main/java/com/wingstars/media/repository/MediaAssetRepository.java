fee1f261550d69e1b095e3f640530a1f664d46d9
台鋼_金小士 <jinxiaoshi24@gmail.com>
2026-04-07 14:09:25 +0800

    feat(media): update media asset repository

--- media/src/main/java/com/wingstars/media/repository/MediaAssetRepository.java
+++ media/src/main/java/com/wingstars/media/repository/MediaAssetRepository.java
@@ -1,6 +1,10 @@
 package com.wingstars.media.repository;
 
 import com.wingstars.media.entity.MediaAsset;
+
+import com.wingstars.media.enums.ModuleSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +12,11 @@ import java.util.Optional;
 
 @Repository
 public interface MediaAssetRepository extends JpaRepository<MediaAsset, Long> {
+    Optional<MediaAsset> findByIdAndIsDeletedFalse(Long id);
+
+    Page<MediaAsset> findAllByIsDeletedFalse(Pageable pageable);
+
+    Page<MediaAsset> findByModuleSourceAndIsDeletedFalse(ModuleSource moduleSource, Pageable pageable);
+
     Optional<MediaAsset> findByFileUrl(String fileUrl);
 }
