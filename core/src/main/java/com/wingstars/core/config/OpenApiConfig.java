91b580d8ecbd1c738a72c6472e50437154a02c17
jinxiaos-金小士 <jinxiaoshi24@gmail.com>
2026-04-09 10:35:40 +0800

    feat(swagger): configure OpenAPI to use relative server URL for ngrok compatibility

--- core/src/main/java/com/wingstars/core/config/OpenApiConfig.java
+++ core/src/main/java/com/wingstars/core/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
