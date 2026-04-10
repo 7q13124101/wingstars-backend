d48ed37420dd85a5a25aa74c166f14f089423447
jinxiaos-金小士 <jinxiaoshi24@gmail.com>
2026-04-10 15:52:17 +0800

    mount betwen local and compose docker

--- core/src/main/java/com/wingstars/core/security/SecurityConfig.java
+++ core/src/main/java/com/wingstars/core/security/SecurityConfig.java
@@ -95,7 +95,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+                configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://hypermiraculous-uncontumaciously-melonie.ngrok-free.dev"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
